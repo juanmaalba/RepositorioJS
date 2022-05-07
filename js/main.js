544de298c2a705603fe5e2413098844ae523b4ba
JuanAlba <jalbaworkspace@gmail.com>
2022-05-07 17:19:22 -0300

    Avance 7/5

--- js/main.js
+++ js/main.js
@@ -1,37 +1,43 @@
-//Seleccion de seguros
-let seguro = prompt(`Seleccionar producto de calidad:
-                                1. Premium
-                                2. Avanzado
-                                3. Medio
-                                4. Bajo
-                                5. Salir` );
-while(seguro != 'Salir'){
-    switch (seguro){
-        case 'Premium':
-            alert('Usted adquirio el paquete Premium');
-            break
-        case 'Avanzado':
-            alert('Usted adquirio el paquete Avanzado');
-            break
-        case 'Medio':
-            alert('Usted adquirio el paquete Medio');
-            break
-        case 'Bajo':
-            alert('Usted adquirio el paquete Bajo');
-            break
-        default:
-            alert('No es una opcion valida')
-    }
-    seguro = prompt(`Seleccionar producto de calidad:
-                                1. Premium
-                                2. Avanazdo
-                                3. Medio
-                                4. Bajo
-                                5. Salir` );
+// Simulador de cuotas sobre tarjeta de credito 
+
+// Ingresar al usuario y contraseña
+
+const usuario = prompt('Ingresar nombre de usuario:')
+if(usuario === 'JuanAlba'){
+    alert('Usuario encontrado')
+}
+else{
+    alert('Usuario no encontrado')
+    const usuario = prompt('Ingresar nombre de usuario:')
+}
+const psw = prompt('Ingresar contraseña:')
+if(psw === '1234'){
+    alert('Contraseña aceptada, bienvenido.')
+    
+}
+else{
+    alert('Contraseña incorrecta')
+    const psw = prompt('Vuelve a ingresar contraseña:')
 }
 
-// Contar hasta numero
-let numero = Number(prompt('Ingresar un numero:'))
-for (let i = 1; i<=numero; i+=1){
-    console.log(numero);
+// Menu  
+//HACER FUNCION DE MENU PARA NO REPETIR CODIGO
+let opcion = Number(prompt(`Ingrese el numero de opcion que desea:
+                                        1. Imprimir resumen tarjeta
+                                        2. Calcular nueva compra
+                                        3.  `))
+
+if (opcion=== 1){
+
 }
+else if (opcion===2){
+
+}
+else if (opcion===3){
+        
+}
+else {
+    alert(`Opcion invalida, inserte un numero que 
+            se encuentre dentro del menu.`)
+}
+// calculo de nueva compra
